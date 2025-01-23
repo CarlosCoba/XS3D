9b259d143b815da907b3fe2d3c2837fe14368ec3
CarlosCoba <carlos.lopezcoba@gmail.com>
2025-01-23 11:57:32 +0800

    v1.0.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("requirements.txt", "r") as r:
 all_packages = find_packages()
 setup(
     name="XS3D",
-    version="1.0.0",
+    version="1.0.1",
     description="A Python tool for modeling circular and non-ciruclar motions via emission lines on datacubes.",
     long_description=readme(),
     keywords="kinematics",
