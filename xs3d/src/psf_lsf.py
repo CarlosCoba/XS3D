b306df550d2ed000e51bd5c5f2fc6cc50ed0eb5b
CarlosCoba <carlos.lopezcoba@gmail.com>
2025-04-01 11:34:45 +0800

    XS3D

--- xs3d/src/psf_lsf.py
+++ xs3d/src/psf_lsf.py
@@ -3,7 +3,7 @@ from .constants import __c__,__sigma_2_FWHM__,__FWHM_2_sigma__
 
 class PsF_LsF:
 	def __init__(self,cube_hdr, config):
-	
+
 		config_general = config['general']
 		header_xs3d =config['header']
 		self.ctype3=header_xs3d.get('ctype3','wavelength')
@@ -14,7 +14,7 @@ class PsF_LsF:
 			self.bmaj=cube_hdr['BMAJ']*3600
 			self.bmin=cube_hdr['BMIN']*3600
 			self.bpa=cube_hdr['BPA']
-			self.fwhm_psf_arc=None			
+			self.fwhm_psf_arc=None
 			self.fit_psf=True
 		except(KeyError):
 			self.fwhm_psf_arc=config_general.getfloat('psf_fwhm',None)
@@ -25,41 +25,40 @@ class PsF_LsF:
 				self.fit_psf=True
 			else:
 				self.fit_psf=False
-		
-				
+
+
 		self.cdelt3=float(cube_hdr['CDELT3'])
-		self.nz=cube_hdr['NAXIS3']		
+		self.nz=cube_hdr['NAXIS3']
 		self.eline_A=config_general.getfloat('eline',None)
 		self.fwhm_inst_A=config_general.getfloat('fwhm_inst',None)
-		#self.fwhm_inst_kms=config_general.getfloat('fwhm_kms',None)		
+		#self.fwhm_inst_kms=config_general.getfloat('fwhm_kms',None)
 		self.sigma_inst_A=self.fwhm_inst_A*__FWHM_2_sigma__ if self.fwhm_inst_A is not None else None
 		self.sigma_inst_kms=(self.sigma_inst_A/self.eline_A)*__c__ if self.fwhm_inst_A is not None else None
-		self.fwhm_inst_kms=self.sigma_inst_kms*__sigma_2_FWHM__ if self.sigma_inst_kms is not None else config_general.getfloat('fwhm_kms',None)  
-		
-		
+		fwhm_inst_kms=config_general.getfloat('fwhm_kms',None)
+		self.fwhm_inst_kms=fwhm_inst_kms if fwhm_inst_kms is not None else self.sigma_inst_kms*__sigma_2_FWHM__  
+
+
 		if 'velocity' in self.ctype3:
 			if 'kms' in self.ctype3:
 				# if not passed fwhm_A then try the config file
 				self.fwhm_inst_kms=self.fwhm_inst_A if self.fwhm_inst_A is not None else  config_general.getfloat('fwhm_kms',None)
-				# start fwhm_A				  
-				self.fwhm_inst_A=self.fwhm_inst_kms				
-				self.sigma_inst_kms=(self.fwhm_inst_kms*__FWHM_2_sigma__) if self.fwhm_inst_A is not None else None							
-				self.sigma_inst_A=1e3*self.fwhm_inst_kms*__FWHM_2_sigma__								
+				# start fwhm_A
+				self.fwhm_inst_A=self.fwhm_inst_kms
+				self.sigma_inst_kms=(self.fwhm_inst_kms*__FWHM_2_sigma__) if self.fwhm_inst_A is not None else None
+				self.sigma_inst_A=1e3*self.fwhm_inst_kms*__FWHM_2_sigma__
 			elif 'ms' in self.ctype3:
 				# if not passed fwhm_A then try the config file
 				self.fwhm_inst_kms=self.fwhm_inst_A/1e3 if self.fwhm_inst_A is not None else  config_general.getfloat('fwhm_kms',None)
-				# start fwhm_A				  
+				# start fwhm_A
 				self.fwhm_inst_A=self.fwhm_inst_kms*1e3
 				self.sigma_inst_kms=(self.fwhm_inst_kms*__FWHM_2_sigma__) if self.fwhm_inst_A is not None else None
-				self.sigma_inst_A=1e3*self.fwhm_inst_kms*__FWHM_2_sigma__								
+				self.sigma_inst_A=1e3*self.fwhm_inst_kms*__FWHM_2_sigma__
 			else:
 				print('XS3D: Not recognized velocity units in CTYPE3')
-				quit() 		
-			
+				quit()
+
 		self.sigma_inst_pix=(self.sigma_inst_A/abs(self.cdelt3))*np.ones(self.nz) if self.fwhm_inst_A is not None else None
-		#print(self.sigma_inst_A, self.cdelt3, self.fwhm_inst_A , self.fwhm_inst_kms, self.sigma_inst_kms, self.sigma_inst_pix);quit()					
+		#print(self.sigma_inst_A, self.cdelt3, self.fwhm_inst_A , self.fwhm_inst_kms, self.sigma_inst_kms, self.sigma_inst_pix);quit()
 		#if self.fwhm_inst_kms is not None:
 		#	self.sigma_inst_kms=self.fwhm_inst_kms*__FWHM_2_sigma__
 		#	self.sigma_inst_pix=self.sigma_inst_kms/abs(self.cdelt3)
-			
-
