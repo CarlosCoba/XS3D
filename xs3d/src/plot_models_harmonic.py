8738888dddad2bead2cfb96d4968722926ed7e5d
CarlosCoba <carlos.lopezcoba@gmail.com>
2025-04-08 16:53:31 +0800

    XS3D

--- xs3d/src/plot_models_harmonic.py
+++ xs3d/src/plot_models_harmonic.py
@@ -6,7 +6,7 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 
 from matplotlib.offsetbox import AnchoredText
-from .axes_params import axes_ambient as axs 
+from .axes_params import axes_ambient as axs
 from .cbar import colorbar as cb
 from .colormaps_CLC import vel_map
 
@@ -20,11 +20,11 @@ def vmin_vmax(data,pmin=5,pmax=98,base=None):
 def zero2nan(data):
 	data[data==0]=np.nan
 	return data
-	
+
 prng =  np.random.RandomState(123)
 
 #params =   {'text.usetex' : True }
-#plt.rcParams.update(params) 
+#plt.rcParams.update(params)
 cmap = vel_map()
 
 # These are ad-hoc colors. I like this.
@@ -35,7 +35,7 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 	c1 = Ck[0]
 	e_c1 = e_Ck[0]
 	s_1 = Sk[0]
-	Rnoncirc = R*(s_1/s_1) 
+	Rnoncirc = R*(s_1/s_1)
 
 
 	width, height = 14.0, 5.5 # width [cm]
@@ -43,7 +43,7 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 	cm_to_inch = 0.393701 # [inch/cm]
 	figWidth = width * cm_to_inch # width [inch]
 	figHeight = height * cm_to_inch # width [inch]
-  
+
 	fig = plt.figure(figsize=(figWidth, figHeight), dpi = 300)
 	#nrows x ncols
 	widths = [1, 1, 0.4,1.5]
@@ -59,33 +59,33 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 
 	# is it the axes in arcsec or arcmin ?
 	rnorm=1
-	if np.max(ext)>80:
+	if np.max(R)>80:
 		rnorm=60
 		rlabel='$\'$'
 	else:
-		rlabel='$\'\'$'	
+		rlabel='$\'\'$'
 	ext = ext/rnorm
 	R=R/rnorm
-	Rnoncirc=Rnoncirc/rnorm	
+	Rnoncirc=Rnoncirc/rnorm
 
 	# intrinsic velocity
 	vt=zero2nan(twoDmodels[0])
-	velmax = 10*(np.nanmax(vt) // 10) 		
+	velmax = 10*(np.nanmax(vt) // 10)
 
 	im0=ax0.imshow(vt,origin='lower',cmap=cmap,extent=ext,vmin=0,vmax=velmax,aspect='auto')
-	
-	
+
+
 	# intrinsic velocity dispersion
 	sigmap_intr=zero2nan(sigmap_intr)
 	vmin = abs(np.nanmin(sigmap_intr))
 	vmax = abs(np.nanmax(sigmap_intr))
-	max_vel = np.nanmax([vmin,vmax])	
+	max_vel = np.nanmax([vmin,vmax])
 	vmin = -(max_vel//25 + 1)*25
 	vmax = (max_vel//25 + 1)*25
 	vmin,vmax=vmin_vmax(sigmap_intr,base=10)
-	im1=ax1.imshow(sigmap_intr,origin='lower',cmap=cmap,extent=ext,vmin=vmin,vmax=vmax,aspect='auto')		
+	im1=ax1.imshow(sigmap_intr,origin='lower',cmap=cmap,extent=ext,vmin=vmin,vmax=vmax,aspect='auto')
 
-	
+
 	axs(ax0)
 	axs(ax1,remove_yticks= True)
 
@@ -101,11 +101,11 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 	ax3.plot(R,c1, color = "#362a1b",linestyle='-', alpha = 1, linewidth=0.8, label = "$\mathrm{c_{1}}$")
 	ax3.fill_between(R, c1-e_c1, c1+e_c1, color = "#362a1b", alpha = 0.3, linewidth = 0)
 	ax3.scatter(R,c1,s=20,marker='s',c='#362a1b',edgecolor='k',lw=0.3,zorder=10)
-		
+
 	ax3.plot(R,Sigma, color = "#db6d52",linestyle='--', alpha = 1, linewidth=0.8, label = "$\sigma_{intr}$")
 	ax3.fill_between(R, Sigma-eSigma, Sigma+eSigma, color = "#db6d52", alpha = 0.3, linewidth = 0)
 	ax3.scatter(R,Sigma,s=20,marker='s',c='#db6d52',edgecolor='k',lw=0.3,zorder=10)
-	
+
 	ax3.set_ylim(0, 30*(np.max(c1)//30)+40)
 	ax3.set_xlim(0, np.max(R))
 	axs(ax3, remove_xticks= True, rotation = 'horizontal')
@@ -136,11 +136,11 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 		for i in range(m_hrm):
 			k1 = prng.randint(0, n-1)
 			k2 = prng.randint(0, n-1)
-			if i >= 1: 
+			if i >= 1:
 				ax4.plot(Rnoncirc,Ck[i], color = colors[k1],linestyle='-', alpha = 1, linewidth=0.8, label = "$\mathrm{c_{%s}}$"%(i+1))
 				ax4.fill_between(Rnoncirc, Ck[i]-e_Ck[i], Ck[i]+e_Ck[i], color = colors[k1], alpha = 0.3, linewidth = 0)
 				ax4.scatter(Rnoncirc,Ck[i],s=4,marker='s',c=colors[k1],edgecolor='k',lw=0.3,zorder=10)
-			
+
 			ax4.plot(Rnoncirc,Sk[i], color = colors[k2],linestyle='-', alpha = 1, linewidth=0.8, label = "$\mathrm{s_{%s}}$"%(i+1))
 			ax4.fill_between(Rnoncirc, Sk[i]-e_Sk[i], Sk[i]+e_Sk[i], color = colors[k2], alpha = 0.3, linewidth = 0)
 			ax4.scatter(Rnoncirc,Sk[i],s=4,marker='s',c=colors[k2],edgecolor='k',lw=0.3,zorder=10)
@@ -164,8 +164,3 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 	ax4.grid(visible = True, which = "major", axis = "both", color='w', linestyle='-', linewidth=0.5, zorder = 1, alpha = 0.5)
 	plt.savefig("%sfigures/kin_%s_disp_%s.png"%(out,vmode,galaxy))#, transparent=True)
 	plt.clf()
-
-
-
-
-
