8893c543955247f8d36cc2100ab58b5f398b4278
carlos <carlos.lopezcoba@gmail.com>
2026-05-20 01:43:03 -0500

    XS3D

--- xs3d/src/plot_models_harmonic.py
+++ xs3d/src/plot_models_harmonic.py
@@ -49,7 +49,7 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 	#nrows x ncols
 	widths = [1, 1, 0.4,1.5]
 	heights = [1, 0.6]
-	gs = gridspec.GridSpec(2, 1)#, height_ratios=heights)
+	gs = gridspec.GridSpec(2, 1), height_ratios=heights)
 	gs.update(left=0.15, right=0.95,top=0.87,bottom=0.14, hspace = 0.05, wspace = 0)
 
 
@@ -60,9 +60,9 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 	rnorm=1
 	if np.max(R)>80:
 		rnorm=60
-		rlabel='$\'$'
+		rlabel='arcmin'
 	else:
-		rlabel='$\'\'$'
+		rlabel='arcsec'
 	ext = ext/rnorm
 	R=R/rnorm
 	Rnoncirc=Rnoncirc/rnorm
@@ -72,17 +72,17 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 	ax0.fill_between(R, c1-e_c1, c1+e_c1, color = "#362a1b", alpha = 0.3, linewidth = 0)
 	ax0.scatter(R,c1,s=20,marker='D',c='#362a1b',edgecolor='k',lw=0.3,zorder=10)
 
-	ax0.plot(R,Sigma, color = "#db6d52",linestyle='--', alpha = 1, linewidth=0.8, label = "$\sigma_{intr}$")
+	ax0.plot(R,Sigma, color = "#db6d52",linestyle='--', alpha = 1, linewidth=0.8, label = "$\sigma_{gas}$")
 	ax0.fill_between(R, Sigma-eSigma, Sigma+eSigma, color = "#db6d52", alpha = 0.3, linewidth = 0)
 	ax0.scatter(R,Sigma,s=20,marker='D',c='#db6d52',edgecolor='k',lw=0.3,zorder=10)
 
 	ax0.set_ylim(0, 30*(np.max(c1)//30)+40)
 	ax0.set_xlim(0, np.max(R))
-	axs(ax0, remove_xticks= True, rotation = 'horizontal', fontsize_ticklabels=18)
+	axs(ax0, remove_xticks= True, rotation = 'horizontal', fontsize_ticklabels=16)
 
 
 	# plot the 1D velocities with the predefined colors
-	ax1.plot(R,-1e4*Sigma, color = "#db6d52",linestyle='--', alpha = 1, linewidth=0.8, label = "$\sigma_{intr}$")
+	ax1.plot(R,-1e4*Sigma, color = "#db6d52",linestyle='--', alpha = 1, linewidth=0.8, label = "$\sigma_{gas}$")
 	ax1.plot(R,-1e4*np.ones(len(c1)), color = "#362a1b",linestyle='-', alpha = 1, linewidth=0.8, label = "$\mathrm{c_{1}}$")
 	ax1.plot([0,np.nanmax(R)],[0,0],color = "k",linestyle='-', alpha = 0.6,linewidth = 0.3)
 	if 2*m_hrm < len(list_fancy_colors):
@@ -115,18 +115,18 @@ def plot_kin_models_h(galaxy,vmode,momms_mdls,R,Sigma,eSigma,Ck,Sk,e_Ck,e_Sk,VSY
 			ax1.fill_between(Rnoncirc, Sk[i]-e_Sk[i], Sk[i]+e_Sk[i], color = colors[k2], alpha = 0.3, linewidth = 0)
 			ax1.scatter(Rnoncirc,Sk[i],s=4,marker='D',c=colors[k2],edgecolor='k',lw=0.3,zorder=10)
 
-	axs(ax1,  rotation = 'horizontal', fontsize_ticklabels=18)
+	axs(ax1,  rotation = 'horizontal', fontsize_ticklabels=16)
 	vmin_s1 = 5*abs(np.nanmin(Sk[0]))//5
 	vmax_s1 = 5*abs(np.nanmax(Sk[0]))//5
 	max_vel_s1 = np.nanmax([vmin_s1,vmax_s1])
 	ax1.set_ylim(-max_vel_s1 -4 , max_vel_s1 + 4)
 	ax1.set_xlim(0, np.max(R))
-	ax1.set_xlabel(f"r ({rlabel})", labelpad = 2, fontsize = 18)
-	ax0.set_ylabel("$\mathrm{c_{1}~(km/s)}$",fontsize = 18,labelpad=2)
-	ax1.set_ylabel("$\mathrm{v_{NC}~(km/s)}$",fontsize = 18,labelpad=2)
+	ax1.set_xlabel(f"r ({rlabel})", labelpad = 2, fontsize = 16)
+	ax0.set_ylabel("$\mathrm{Velocity~(km/s)}$",fontsize = 16,labelpad=2)
+	ax1.set_ylabel("$\mathrm{V_{NC}~(km/s)}$",fontsize = 16,labelpad=2)
 
 
-	ax1.legend(loc = "center", fontsize = 16, bbox_to_anchor = (0, 2.1, 1.1, 0.2), ncol = m_hrm+1, frameon = False, labelspacing=0.1, handlelength=1, handletextpad=0.3,columnspacing=0.8)
+	ax1.legend(loc = "center", fontsize = 13, bbox_to_anchor = (0.5, 1.15), bbox_transform=ax0.transAxes, ncol = m_hrm+1, frameon = 1, labelspacing=0.1, handlelength=1, handletextpad=0.3,columnspacing=0.8)
 	ax1.grid(visible = True, which = "major", axis = "both", color='w', linestyle='-', linewidth=0.5, zorder = 1, alpha = 0.5)
 
 	#fig.tight_layout()
