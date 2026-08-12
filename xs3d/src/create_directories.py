b7d332c0ed187441549d1ee65dbe303c73ec6c06
carlos <carlos.lopezcoba@gmail.com>
2026-08-12 15:09:14 -0500

    XS3D

--- xs3d/src/create_directories.py
+++ xs3d/src/create_directories.py
@@ -16,6 +16,7 @@ def direc_out(config):
 	path_models = "%smodels/"%main_dir
 	path_plots = "%sfigures/"%main_dir
 
+	'''
 	if path.exists(main_dir) == False:
 		os.mkdir(main_dir)
 
@@ -24,6 +25,17 @@ def direc_out(config):
 
 	if path.exists(path_models) == False:
 		os.mkdir(path_models)
+	'''
+
+	# An FileExistsError is raised if xs3d is run in parallel,
+	# since multiple jobs try to simultaneously create the
+	# directories.
+	# Implementing Christian suggestion 08/12/2026.
+	# Setting exist_ok=True ignores FileExistsError if the
+	# target directory already.
+	os.makedirs(main_dir, exist_ok=True)
+	os.makedirs(path_plots, exist_ok=True)
+	os.makedirs(path_models, exist_ok=True)		
 
 	return main_dir
 
